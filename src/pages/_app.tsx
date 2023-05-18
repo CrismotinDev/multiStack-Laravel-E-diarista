be1c1e8a564bb5dbd3629f3c982715f0a5587974
Cristiane Motin <cristiane.motin@rpinfo.com.br>
2023-05-18 17:00:42 -0300

    front react

---
+++ src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import { ThemeProvider } from "@mui/material";
+import "../ui/styles/globals.css";
+import type { AppProps } from "next/app";
+import theme from "ui/themes/theme";
+import Head from "next/head";
+import Header from "@/ui/components/surfaces/Header/Header";
+import Footer from "@/ui/components/surfaces/Footer/Footer";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>e-diaristas</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/fonts" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
+}
